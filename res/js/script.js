872e7328e6508a53cb57571e08a8b3275d9c64de
Kristjan Pekk <kristjan.pekk@cyber.ee>
2022-07-27 18:16:11 +0300

    dnd dice roller v0.3

--- res/js/script.js
+++ res/js/script.js
@@ -33,6 +33,32 @@ function addDice() {
     amount.setAttribute("type", "number");
     tag.appendChild(amount);
 
+
+    //advantage
+    var advantageText = document.createTextNode("advantage: ");
+    tag.appendChild(advantageText);
+    var advantage = document.createElement("INPUT");
+    advantage.setAttribute("id", "advantage");
+    advantage.setAttribute("type", "checkbox");
+    tag.appendChild(advantage);
+
+    //disadvantage
+    var disadvantageText = document.createTextNode("disadvantage: ");
+    tag.appendChild(disadvantageText);
+    var disadvantage = document.createElement("INPUT");
+    disadvantage.setAttribute("id", "disadvantage");
+    disadvantage.setAttribute("type", "checkbox");
+    tag.appendChild(disadvantage);
+
+    //modifier
+    var modifierText = document.createTextNode("Modifier: ");
+    tag.appendChild(modifierText);
+
+    var modifier = document.createElement("input");
+    modifier.setAttribute("id", "modifier");
+    modifier.setAttribute("type", "number");
+    tag.appendChild(modifier);
+
     // bloki kustutamise nupp
     var deleteBtn = document.createElement("button");
     deleteBtn.innerHTML = "X";
@@ -50,7 +76,7 @@ function addDice() {
 function randomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
-    for (var i = 0; i < 6; i++){
+    for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
@@ -63,16 +89,29 @@ function calculateThrows() {
 
     let dices = document.getElementById("dices").childNodes;
 
+    var total = 0;
+    var diceTotal = 0;
+
     // iga täringu kohta:
     dices.forEach(dice => {
 
+        // lähtsestame täringu kogusumma
+        diceTotal = 0;
+
         // täringu küljed
         var sides = dice.querySelector("#sides").value;
-        //console.log("külgi: " + dice.querySelector("#sides").value);
 
         // visete arv
         var throws = dice.querySelector("#amount").value;
-        //console.log("viskeid: " + dice.querySelector("#amount").value);
+
+        // modifier
+        var modifier = dice.querySelector("#modifier").value;
+
+        // advantage
+        var advantage = dice.querySelector("#advantage");
+
+        // disadvantage
+        var disadvantage = dice.querySelector("#disadvantage");
 
         // visete värv (eristamiseks)
         var diceColor = randomColor();
@@ -84,14 +123,48 @@ function calculateThrows() {
 
                 var tag = document.createElement("p");
                 tag.style.background = diceColor;
+
+
                 var dmg = Math.floor(Math.random() * (sides) + 1);
+                // advantage
+                if (advantage.checked) {
+                    dmg = Math.max(Math.floor(Math.random() * (sides) + 1), Math.floor(Math.random() * (sides) + 1));
+                };
+
+                // disadvantage
+                if (disadvantage.checked) {
+                    dmg = Math.min(Math.floor(Math.random() * (sides) + 1), Math.floor(Math.random() * (sides) + 1));
+                };
+
+                // dmg +- modifier
+                if (modifier) {
+                    dmg += +modifier;
+                };
+
+                total += +dmg;
+                diceTotal += +dmg;
+
                 var text = document.createTextNode("dmg: " + dmg);
                 tag.appendChild(text);
                 var element = document.getElementById("output");
                 element.appendChild(tag);
             }
+
+            var tag = document.createElement("p");
+            var text = document.createTextNode("dice dmg: " + diceTotal);
+            tag.appendChild(text);
+            tag.style.background = "gray";
+            var element = document.getElementById("output");
+            element.appendChild(tag);
         }
     })
+
+    var tag = document.createElement("p");
+    var text = document.createTextNode("total dmg: " + total);
+    tag.appendChild(text);
+    tag.style.background = "gray";
+    var element = document.getElementById("output");
+    element.appendChild(tag);
 
     //todo total
 }
